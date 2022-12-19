9393bec982768a9c52c015973aea92818e9433a3
Leotrim <88747853+trimooo@users.noreply.github.com>
2022-12-20 00:04:12 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
   $hostname = "localhost";
   $username = "root";
-  $password = "root";
+  $password = "";
   $dbname = "chat";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
